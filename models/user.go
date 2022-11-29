b8c8120567c3643eb779823149bf8d99633818a4
Ben Eich <jgradio64@yahoo.com>
2022-11-28 21:45:28 -0500

    Add method to get posts from followed users for home

--- models/user.go
+++ models/user.go
@@ -2,7 +2,7 @@ package models
 
 import (
 	"database/sql"
-
+	
 	extns "blbr.com/main/extensions"
 
     _ "github.com/lib/pq"
@@ -36,6 +36,7 @@ func (u User) GetUser() User {
 	 FROM posts WHERE posts.user_id=$1 
 	 ORDER BY post_date DESC LIMIT 10`
 	rows, err := db.Query(queryString, u.UserID)
+	defer rows.Close()
 	for rows.Next() {
 		var post Post
 		err = rows.Scan(&post.PostID, &post.UserID, &post.PostContent, &post.PostTitle, &post.PostDate, &post.EditDate)
@@ -81,4 +82,42 @@ func (u User) DeleteUser() {
 	deleteStatement := `DELETE FROM users WHERE user_id=$1`
 	_, err = db.Exec(deleteStatement, u.UserID)
 	extns.CheckError(err)
+}
+
+func (u User) GetHomePagePosts() []Post {
+	connectionString := extns.GetEnvVariable("CONNECTIONSTRING")
+    db, err := sql.Open("postgres", connectionString)
+	extns.CheckError(err)
+	defer db.Close()
+	var followedUserPosts []Post
+
+	queryStatement := `SELECT posts.post_id, posts.user_id, posts.post_content, posts.post_title, posts.post_date, posts.edit_date, users.user_name 
+	FROM posts INNER JOIN users ON posts.user_id=users.user_id 
+	WHERE posts.user_id IN (SELECT followed_user_id FROM followed_users WHERE user_id=$1)
+	ORDER BY post_date DESC LIMIT 10`
+	rows, err := db.Query(queryStatement, u.UserID)
+	extns.CheckError(err)
+	defer rows.Close()
+
+	for rows.Next() {
+		var post Post
+		err = rows.Scan(&post.PostID, &post.UserID, &post.PostContent, &post.PostTitle, &post.PostDate, &post.EditDate, &post.Author)
+		extns.CheckError(err)
+
+		// Gets the number of likes on a post
+		numLikesQuery := `SELECT COUNT(user_id) FROM post_likes WHERE post_id=$1`
+		row := db.QueryRow(numLikesQuery, post.PostID)
+		err = row.Scan(&post.NumOfLikes)
+		extns.CheckError(err)
+
+		// Gets the number of comments on a post
+		numPostsQuery := `SELECT COUNT(comment_id) FROM comments WHERE post_id=$1`
+		row = db.QueryRow(numPostsQuery, &post.PostID)
+		err = row.Scan(&post.NumComments)
+		extns.CheckError(err)
+
+		followedUserPosts = append(followedUserPosts, post)
+	}
+
+	return followedUserPosts
 }
