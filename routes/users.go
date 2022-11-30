af9fe2c0c507301c39e07800ebdcb089cb8c0416
Ben Eich <jgradio64@yahoo.com>
2022-11-30 03:41:49 -0500

    Add CRUD functions for over routes, and templates

---
+++ routes/users.go
@@ -0,0 +1,176 @@
+package routes
+ 
+import (
+    "fmt"
+	"net/http"
+	"time"
+	"html/template"
+	"strconv"
+	models "blbr.com/main/models"
+	extns "blbr.com/main/extensions"
+)
+
+type UserData struct {
+	IsOwner		bool
+	IsAuth		bool
+	AuthUserID	int
+	TheUser		models.User
+}
+
+var templates = template.Must(template.ParseGlob("pages/*"))
+
+
+func GetUserHandler(w http.ResponseWriter, r *http.Request) {
+	// Base get route without any parameters will return the user's own profile
+	// Get the user's id from the url parameters
+	isAuthenticated := CheckSession(w, r) 
+	if isAuthenticated {
+		queryId := r.URL.Query().Get("id")
+		c, _ := r.Cookie("session_token")
+		sessionToken := c.Value
+		userSession := sessions[sessionToken]
+
+		id, err := strconv.Atoi(queryId)
+		extns.CheckError(err)
+
+		user  := models.User{UserID: id}
+		user = user.GetUser()
+
+		if id == userSession.UserID {
+			newUserData := UserData{IsOwner: true, TheUser: user, IsAuth: isAuthenticated, AuthUserID: userSession.UserID }
+			page := &newUserData
+			renderUserTemplate(w, "userprofile", page)
+		} else {
+			newUserData := UserData{IsOwner: false, TheUser: user, IsAuth: isAuthenticated, AuthUserID: userSession.UserID }
+			page := &newUserData
+			renderUserTemplate(w, "userprofile", page)
+		}
+	} else {
+		Backtologin(w, r)
+	}
+}
+
+func EditUserHandler(w http.ResponseWriter, r *http.Request) {
+	isAuthenticated := CheckSession(w, r) 
+	if isAuthenticated {
+		c, _ := r.Cookie("session_token")
+		sessionToken := c.Value
+		userSession := sessions[sessionToken]
+
+		user := models.User{UserID: userSession.UserID, Username: userSession.Username}
+		user = user.GetUser()
+
+		newUserData := UserData{IsOwner: true, TheUser: user, IsAuth: isAuthenticated, AuthUserID: userSession.UserID}
+		page := &newUserData
+
+		renderUserTemplate(w, "edituser", page)
+	} else {
+		Backtologin(w, r)
+	}
+}
+
+func DeleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	isAuthenticated := CheckSession(w, r) 
+	if isAuthenticated {
+		issueList, err := template.ParseFiles("pages/user_delete.html")
+		extns.CheckError(err)
+		
+		err = issueList.Execute(w, nil)
+		extns.CheckError(err)
+	} else {
+		Backtologin(w, r)
+	}
+}
+
+func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
+	var newUser models.User
+
+	newUser.Username = r.FormValue("Username")
+    newUser.Password = r.FormValue("Password")	
+	newUser.AboutUser = r.FormValue("About")
+
+	// Runs a query to check if the username is taken
+	userAlreadyExists := newUser.CheckUserNameExists()
+	
+	if userAlreadyExists {
+		fmt.Fprintf(w, "<h1> That username already exits. Try a new one. </h1>")
+	} else {
+		
+		fmt.Fprintf(w, "<h1> User %s Created Successfully!</h1>" + "<p>Now go to the log in page...</p>", newUser.Username)
+		newUser.CreateUser()
+		
+		
+		if f, ok := w.(http.Flusher); ok {
+			f.Flush()
+			time.Sleep(1 * time.Second)
+		}
+		
+
+		fmt.Fprintf(w, `<script>window.location.href="/";</script>`) 
+	} 
+}
+
+func EditUserConfirmHandler(w http.ResponseWriter, r *http.Request) {
+	isAuthenticated := CheckSession(w, r) 
+	if isAuthenticated {
+		about := r.FormValue("About")
+		c, _ := r.Cookie("session_token")
+		sessionToken := c.Value
+		userSession := sessions[sessionToken]
+
+		user := models.User{UserID: userSession.UserID, AboutUser: about}
+		user.UpdateUser()
+
+		RefreshSession(w, r)
+
+		fmt.Fprintf(w, "<h1> Profile Update Successful!</h1>" + "<p>Now go to the user homepage...</p>")
+	
+		if f, ok := w.(http.Flusher); ok {
+			f.Flush()
+			time.Sleep(1 * time.Second)
+		}
+			
+		fmt.Fprintf(w, `<script>window.location.href="/homepage";</script>`) 
+	} else {
+		Backtologin(w, r)
+	}
+}
+
+func DeleteUserConfirmHandler(w http.ResponseWriter, r *http.Request) {
+	isAuthenticated := CheckSession(w, r) 
+	if isAuthenticated {
+		c, _ := r.Cookie("session_token")
+		sessionToken := c.Value
+		userSession := sessions[sessionToken]
+
+		user := models.User{UserID: userSession.UserID}
+		user.DeleteUser()
+
+		delete(sessions, c.Value)
+
+		http.SetCookie(w, &http.Cookie{
+			Name:    "session_token",
+			Value:   "",
+			Expires: time.Now(),
+		})
+
+		fmt.Fprintf(w, "<h1>Profile Deletion Successful!</h1>" + "<p>See you in the future. Maybe?</p>")
+	
+		if f, ok := w.(http.Flusher); ok {
+			f.Flush()
+			time.Sleep(1 * time.Second)
+		}
+			
+		fmt.Fprintf(w, `<script>window.location.href="/";</script>`) 
+	} else {
+		Backtologin(w, r)
+	}
+}
+
+func renderUserTemplate(w http.ResponseWriter, tmpl string, page *UserData) {
+	err := templates.ExecuteTemplate(w, tmpl, page)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
